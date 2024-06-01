5647da84db0e9f1b7b9b4d0f71812e007381d850
f3ath <karapetov@gmail.com>
2024-05-31 20:28:07 -0700

    Cleanup

--- bin/check_coverage.dart
+++ bin/check_coverage.dart
@@ -1,26 +1,13 @@
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:check_coverage/src/trace_file.dart';
+import 'package:check_coverage/src/app.dart';
 
 Future<void> main(List<String> args) async {
   try {
     final lines = stdin.transform(utf8.decoder).transform(const LineSplitter());
-    final traceFile = await TraceFile.readLines(lines);
-    final expected = args.isEmpty ? 100 : num.parse(args.first);
-    final coverage = (100 * traceFile.coveredRatio).floor();
-    if (coverage < expected) {
-      stderr.writeln(
-          'Total coverage of $coverage% is below expected $expected%.');
-      stderr.writeln('Top uncovered files:');
-      traceFile.uncovered.take(3).forEach((file) {
-        stderr.writeln(file.name);
-        stderr.writeln(
-            'Lines (${file.uncovered.length}): ${file.uncoveredRanges.join(', ')}');
-      });
-
-      exit(1);
-    }
+    final code = await app(args, lines, stderr.writeln);
+    exit(code);
   } on Object catch (e) {
     stderr.writeln(e);
     exit(128);
